efdb326e5e3add4a1ff05698e683d35beb3c5e94
laubonghaudoi <laubonghaudoi@icloud.com>
2025-02-12 10:32:15 -0800

    改進 SEO

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const cantoneseInput = document.getElementById('cantoneseInput');
     const jyutpingOutput = document.getElementById('jyutpingOutput');
     const ipaOutput = document.getElementById('ipaOutput');
-    const convertButton = document.getElementById('convertButton');
 
-    convertButton.addEventListener('click', () => {
+    cantoneseInput.addEventListener('input', () => {
         jyutpingOutput.value = ToJyutping.getJyutpingText(cantoneseInput.value);
         ipaOutput.value = ToJyutping.getIPAText(cantoneseInput.value);
     });
